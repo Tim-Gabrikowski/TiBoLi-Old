fc3dc5ffd3286729b472444ac982705e0fbb2999
Jens Gabrikowski <jens@gabrikowski.de>
2021-10-10 11:59:57 +0200

    Get books from mysql database now

--- booksapi/service/BooksService.ts
+++ booksapi/service/BooksService.ts
@@ -1,17 +1,24 @@
- export default class BooksService {
+let mysql = require('promise-mysql');
+
+export default class BooksService {
     constructor() {
         console.log('constructor');
     }
+
     getBooks() {
+        return this.selectFromBooks();
+    }
 
-        return new Promise<any[]>((resolve, reject) => {
+    private async selectFromBooks() {
+        const connectionOptions = {
+            host: 'db',
+            user: 'root',
+            password: 's3cret',
+            database: 'tiboli',
+        };
+        let connection = await mysql.createConnection(connectionOptions);
+        let books = await connection.query('SELECT * from books;')
 
-            setTimeout(() => {
-                resolve([
-                    {id: 1, name: 'book1'},
-                    {id: 2, name: 'book2'},
-                ]);
-            }, 10);
-        });
+        return books;
     }
 }
